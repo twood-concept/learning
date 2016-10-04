328a4d1863bcd7c6aa72a789ae3a1396b91ccecd
twood-concept <twood@albany.conceptsystemsinc.com>
2016-10-04 10:37:18 -0700

    Initial setup done

--- 1_react/App.js
+++ 1_react/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 class App extends React.Component {
     render(){
-        return <div>Hello ish</div>
+        return <div>Hello Tyler</div>
     }
 }
 export default App
