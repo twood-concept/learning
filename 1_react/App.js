641612d8e26592a690278af9d9f8b29fedbae7dd
twood-concept <twood@albany.conceptsystemsinc.com>
2016-10-04 10:46:38 -0700

    Just playing around

--- 1_react/App.js
+++ 1_react/App.js
@@ -1,7 +1,23 @@
 import React from 'react';
+
 class App extends React.Component {
     render(){
-        return <div>Hello Tyler</div>
+        return (
+            <div>
+                <h1>Hello Sir</h1>
+                <b>Bold</b>
+            </div>
+        );
     }
 }
+
+// class App extends React.Component {
+//     render(){
+//         // return React.createElement('h1', null, 'Hello Tyler')
+//         return <h1>Hello Sir</h1>
+//     }
+// }
+
+// const App = () => <h1>Hello Mister</h1>
+
 export default App
