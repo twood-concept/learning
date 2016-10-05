e64748d03bc0bf8f87743b83c1304487360c1dee
twood-concept <twood@albany.conceptsystemsinc.com>
2016-10-04 17:03:55 -0700

    Some changes?

--- 1_react/App.js
+++ 1_react/App.js
@@ -1,15 +1,28 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 class App extends React.Component {
     render(){
-        return (
-            <div>
-                <h1>Hello Sir</h1>
-                <b>Bold</b>
-            </div>
-        );
+        let txt = this.props.txt
+        return <h1>{txt}</h1>
     }
 }
+
+App.proTypes = {
+    txt: React.PropTypes.string,
+    cat: React.PropTypes.number.isRequired
+}
+
+App.defaultProps = {
+    txt: 'this is the default'
+}
+
+ReactDOM.render(
+    <App cat={5} txt="this is the props text" />,
+    document.getElementById('app')
+);
+
+console.log('test 2');
 
 // class App extends React.Component {
 //     render(){
